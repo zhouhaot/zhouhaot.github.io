cb1166a92380e4d43962e62e7e291e7058dad36f
zhou <1330582146@qq.com>
2026-08-30 15:16:01 +0800

    fix: canonical site URL to https://zevth.work (RSS/sitemap/CMS)

--- astro.config.mjs
+++ astro.config.mjs
@@ -26,7 +26,7 @@ import { pluginCustomCopyButton } from "./src/plugins/expressive-code/custom-cop
 
 // https://astro.build/config
 export default defineConfig({
-	site: "https://zhouhaot.github.io/",
+	site: "https://zevth.work/",
 	base: "/",
 	trailingSlash: "always",
 	integrations: [
